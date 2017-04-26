8f6e32a41da3a8fab042c2463d993a701cdad6ad
Federico Dolce <psykopear@gmail.com>
2017-04-26 12:27:58 +0200

    Fix tests

--- tests/test_libtrantor.py
+++ tests/test_libtrantor.py
@@ -1,4 +1,5 @@
 from trantor import parse, build
+from datetime import datetime
 
 
 def test_parse():
@@ -24,14 +25,14 @@ def test_parse():
         'giroscope_y': 8, 'giroscope_z': 4, 'hdop': 2, 'lat': -12.1324,
         'lon': 43.325, 'magnetometer_x': 2, 'magnetometer_y': 4,
         'magnetometer_z': 1, 'pdop': 1, 'satellites': 6, 'speed': 123,
-        'tdop': 1, 'temperature': 4, 'timestamp': 16777472, 'vdop': 2}
+        'tdop': 1, 'temperature': 4, 'timestamp': datetime.fromtimestamp(16777472), 'vdop': 2}
     expected_data_2 = {
         'acc_x': 0, 'acc_y': 0, 'acc_z': 0, 'barometer': 0, 'course': 0,
         'elevation': 1, 'fix': 0, 'geoid_height': 1, 'giroscope_x': 0,
         'giroscope_y': 0, 'giroscope_z': 0, 'hdop': 1, 'lat': 0.00, 'lon': 0.0,
         'magnetometer_x': 0, 'magnetometer_y': 1, 'magnetometer_z': 1, 'pdop': 0,
         'satellites': 1, 'speed': 0, 'tdop': 0, 'temperature': 1,
-        'timestamp': 0, 'vdop': 0}
+        'timestamp': datetime.fromtimestamp(0), 'vdop': 0}
     parsed = list(parse(binary_data))
     assert len(parsed) == 2
     assert parsed[0] == expected_data_1
@@ -48,7 +49,7 @@ def test_build():
         'giroscope_y': 8, 'giroscope_z': 4, 'hdop': 2, 'lat': -12.1324,
         'lon': 43.325, 'magnetometer_x': 2, 'magnetometer_y': 4,
         'magnetometer_z': 1, 'pdop': 1, 'satellites': 6, 'speed': 123,
-        'tdop': 1, 'temperature': 4, 'timestamp': 16777472, 'vdop': 2}
+        'tdop': 1, 'temperature': 4, 'timestamp': datetime.fromtimestamp(16777472), 'vdop': 2}
     built = build(**data)
     print(built)
     binary_data = b'\n\x00\n\x00\xc0(C\xc9\xee\xcb\xfb\x16@E\xa9'\
@@ -70,6 +71,6 @@ def test_both():
         'giroscope_y': 1, 'giroscope_z': 5, 'hdop': 12, 'lat': -12.134243,
         'lon': 43.123432, 'magnetometer_x': 4, 'magnetometer_y': 3,
         'magnetometer_z': 12, 'pdop': 3, 'satellites': 6, 'speed': 2,
-        'tdop': 45, 'temperature': 12, 'timestamp': 1677747.2, 'vdop': 2}
+        'tdop': 45, 'temperature': 12, 'timestamp': datetime.fromtimestamp(1677747.2), 'vdop': 2}
     binary = build(**data)
     assert data == list(parse(binary))[0]
